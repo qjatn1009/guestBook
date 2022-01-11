f6630a09ddce6b1fc63063eac0bf890b0b731c34
qjatn1009 <a43727702@gmail.com>
2022-01-11 15:49:11 +0900

    TEST : Guestbook Service test

---
+++ src/test/java/com/zerock/guestbook/service/GuestbookServiceTests.java
@@ -0,0 +1,40 @@
+package com.zerock.guestbook.service;
+
+import com.zerock.guestbook.dto.GuestbookDTO;
+import com.zerock.guestbook.dto.PageRequestDTO;
+import com.zerock.guestbook.dto.PageResultDTO;
+import com.zerock.guestbook.entity.Guestbook;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GuestbookServiceTests {
+
+    @Autowired
+    private GuestbookService guestbookService;
+
+    @Test
+    public void testRegister(){
+
+        GuestbookDTO guestbookDTO = GuestbookDTO.builder()
+                .title("Sample Title...")
+                .content("Sample Content...")
+                .writer("user0")
+                .build();
+
+        System.out.println(guestbookService.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList() {
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = guestbookService.getList(pageRequestDTO);
+
+        for (GuestbookDTO guestbookDTO : resultDTO.getDtoList()) {
+            System.out.println(guestbookDTO);
+        }
+    }
+}
