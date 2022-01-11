539f87c888450b79a6327169a56241ac4eddced5
qjatn1009 <a43727702@gmail.com>
2022-01-11 11:31:39 +0900

    FEAT : Add Guestbook entity and repository

---
+++ src/main/java/com/zerock/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,7 @@
+package com.zerock.guestbook.repository;
+
+import com.zerock.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+}
