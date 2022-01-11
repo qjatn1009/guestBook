f492bd7a7c16cbcf92c9d5b5ac5635b21907b0b4
qjatn1009 <a43727702@gmail.com>
2022-01-11 17:01:35 +0900

    FEAT : Add Guestbook Controller

--- src/main/java/com/zerock/guestbook/controller/GuestbookController.java
+++ src/main/java/com/zerock/guestbook/controller/GuestbookController.java
@@ -1,21 +1,34 @@
 package com.zerock.guestbook.controller;
 
+import com.zerock.guestbook.dto.PageRequestDTO;
+import com.zerock.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/guestbook")
-@Slf4j
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
+    private final GuestbookService guestbookService;
 
-        log.info("list..........");
+    @GetMapping("/")
+    public String index() {
 
-        return "/guestbook/list";
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("{}", "list......" + pageRequestDTO);
+
+        model.addAttribute("result", guestbookService.getList(pageRequestDTO));
     }
 
 }
